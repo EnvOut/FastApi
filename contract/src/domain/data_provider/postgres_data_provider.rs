c4629c275a9faf1a21759368cbfa67fdb25fab15
Sergey Murzin <sergey.murzin@dataart.com>
2020-02-24 14:06:53 +0200

    cleaned code

--- contract/src/domain/data_provider/postgres_data_provider.rs
+++ contract/src/domain/data_provider/postgres_data_provider.rs
@@ -47,14 +47,6 @@ mod tests {
 
         let mut client = config.connect(NoTls).unwrap();
 
-        // client.batch_execute("
-        //     CREATE TABLE person (
-        //         id      SERIAL PRIMARY KEY,
-        //         name    TEXT NOT NULL,
-        //         data    BYTEA
-        //     )
-        // ").unwrap();
-
         let name = "Ferris";
         let data = None::<&[u8]>;
 
@@ -62,16 +54,6 @@ mod tests {
             "INSERT INTO person (name, data) VALUES ($1, $2)",
             &[&name, &data],
         );
-
-        // for row in client.query("SELECT id, name, data FROM person", &[]).unwrap() {
-        //     println!("{:?}", row.into());
-        //
-        //     let id: i32 = row.get(0);
-        //     let name: &str = row.get(1);
-        //     let data: Option<&[u8]> = row.get(2);
-        //
-        //     println!("found person: {} {} {:?}", id, name, data);
-        // }
 
         {
             let query = "SELECT id, name, data FROM person";
@@ -84,11 +66,3 @@ mod tests {
         }
     }
 }
-
-
-// fn to_value<'a, T: postgres::types::FromSql<'a>>(row: &Row, idx: usize) -> T {
-//     row.get::<usize, T>(idx)
-//     // let value = row.get::<usize, T>(stream);
-//     // let json_value = json!(value);
-//     // Ok(json_value)
-// }
